6aa6bb0381f7f73acadc2b75f38d82f71593c9fa
Sajid Khan <sajidk25@gmail.com>
2024-06-04 19:50:22 +0600

    problem-4 debugged

--- problem_4.php
+++ problem_4.php
@@ -1,18 +1,19 @@
 <?php
-function printPattern($n) {
-    for ($i = 1; $i <= $n; $i++) {
-        // Print spaces
-        for ($j = 1; $j <= $n - $i; $j++) {
-            echo " ";
-        }
-        // Print stars
-        for ($k = 1; $k <= 2 * $i - 1; $k++) {
-            echo "*";
-        }
-        echo "\n";
-    }
-}
+	function printPattern($n){
+	  for ($i=1; $i<=$n; $i++){
+	    for($j=1; $j<=$n-$i; $j++){
+	      echo " ";
+	    }
+	    for($k=1; $k<=2 * $i -1; $k++){
+	      echo "*";
+	    }
+	    echo "\n";
+	  }
 
-$n = 25;
-printPattern($n);
+	}
+
+	echo "Enter a number: ";
+	$input=trim(fgets(STDIN));
+
+	printPattern($input);
 ?>
