0e9dcb30b114e59f27cc9839c04698d6b7089d5f
Sajid Khan <sajidk25@gmail.com>
2024-06-06 22:08:28 +0600

    Problem-3 modified

--- problem_3.php
+++ problem_3.php
@@ -1,16 +1,20 @@
 <?php
 
 function reverseEveryWordsInSentence($input){
+
   $word_array=explode(' ',$input);
-  $abs_callback= fn (string $word)=>strrev($word);
-  $reverse_words_Array= array_map($abs_callback,$word_array);
+  $strrev_callback= fn (string $word)=>strrev($word);
+  $reverse_words_Array= array_map($strrev_callback,$word_array);
   // var_dump($reverse_words_Array);
   $reverse_words_sentence=implode(' ',$reverse_words_Array);
   return $reverse_words_sentence;
 }
 
-echo "Enter a sentence: ";
-$input=trim(fgets(STDIN));
+$input='';
+while (!$input) {
+  echo "Enter a sentence: ";
+  $input=trim(fgets(STDIN));
+}
 
 echo "Reverse word sentence: ". reverseEveryWordsInSentence($input)."\n";
 
